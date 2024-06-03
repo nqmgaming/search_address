00f594d145fab416cd92b54eb1cacc5d972c951d
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-03 22:18:09 +0700

    fix not build on github

--- app/src/main/java/com/nqmgaming/searchaddresslab/core/di/AppModule.kt
+++ app/src/main/java/com/nqmgaming/searchaddresslab/core/di/AppModule.kt
@@ -15,12 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder()
-            .build()
-    }
 
     @Provides
     @Singleton
