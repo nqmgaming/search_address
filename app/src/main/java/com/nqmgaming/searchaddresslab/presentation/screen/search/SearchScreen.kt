225538386d938cf74555f51bfc94d76f684adc34
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-03 16:12:56 +0700

    edit ui

--- app/src/main/java/com/nqmgaming/searchaddresslab/presentation/screen/search/SearchScreen.kt
+++ app/src/main/java/com/nqmgaming/searchaddresslab/presentation/screen/search/SearchScreen.kt
@@ -1,18 +1,35 @@
 package com.nqmgaming.searchaddresslab.presentation.screen.search
 
 import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.nqmgaming.searchaddresslab.presentation.screen.search.components.AddressItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
     modifier: Modifier = Modifier,
@@ -41,19 +58,63 @@ fun SearchScreen(
             onValueChange = {
                 viewModel.onChangeQuery(it)
                 viewModel.debounceQuery(coroutineScope, it)
-            }
+            },
+            leadingIcon = {
+                if (isSearchLoading || isLoading) {
+                    CircularProgressIndicator()
+                } else {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search here"
+                    )
+                }
+            },
+            trailingIcon = {
+                if (query.isNotEmpty()) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Search here",
+                        modifier = Modifier
+                            .padding(end = 8.dp)
+                            .clickable {
+                                viewModel.onChangeQuery("")
+                                viewModel.debounceQuery(coroutineScope, "")
+                            }
+                    )
+                }
+
+            },
+            shape = RoundedCornerShape(30.dp),
+            maxLines = 1,
+            placeholder = { Text(text = "Search") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .border(
+                    width = 1.dp,
+                    color = Color.Gray.copy(alpha = 0.1f),
+                    shape = RoundedCornerShape(30.dp)
+                ),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                cursorColor = MaterialTheme.colorScheme.onSurface
+            )
         )
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 8.dp)
+        ) {
             addresses.items?.let {
                 items(it.size) { index ->
-                    val address = it[index].address
+                    val address = it[index]
                     AddressItem(
-                        address = address ?: return@items
+                        item = address,
+                        query = query
                     )
                 }
             }
         }
     }
-
-
 }
--- app/src/main/java/com/nqmgaming/searchaddresslab/presentation/screen/search/components/AddressItem.kt
+++ app/src/main/java/com/nqmgaming/searchaddresslab/presentation/screen/search/components/AddressItem.kt
@@ -1,6 +1,8 @@
 package com.nqmgaming.searchaddresslab.presentation.screen.search.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.PlayArrow
@@ -9,25 +11,65 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.unit.dp
 import com.nqmgaming.searchaddresslab.domain.model.Address
+import com.nqmgaming.searchaddresslab.domain.model.Item
 
 @Composable
 fun AddressItem(
     modifier: Modifier = Modifier,
-    address: Address
+    item: Item,
+    query: String
 ) {
-    Row {
+    Row(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+    ) {
         Icon(
             imageVector = Icons.Default.LocationOn,
             contentDescription = "LocationOn"
         )
-        Text(
-            text = address.label ?: "",
-            style = MaterialTheme.typography.labelMedium
-        )
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            // If title is same as query, then highlight the keyword
+            val title = item.title
+            val titleSpanStyle = if (title!!.contains(query, ignoreCase = true)) {
+                SpanStyle(color = Color.Red)
+            } else {
+                SpanStyle()
+            }
+            Text(
+                text = annotateRecursively(
+                    placeHolderList = listOf(Pair(query, titleSpanStyle)),
+                    originalText = title
+                )
+            )
+        }
         Icon(
             imageVector = Icons.Default.PlayArrow,
             contentDescription = "LocationOn"
         )
     }
+}
+
+@Composable
+fun annotateRecursively(
+    placeHolderList: List<Pair<String, SpanStyle>>,
+    originalText: String
+): AnnotatedString {
+    var annotatedString = buildAnnotatedString { append(originalText) }
+    for (item in placeHolderList) {
+        annotatedString = buildAnnotatedString {
+            val startIndex = annotatedString.indexOf(item.first)
+            val endIndex = startIndex + item.first.length
+            append(annotatedString)
+            addStyle(style = item.second, start = startIndex, end = endIndex)
+        }
+    }
+    return annotatedString
 }
