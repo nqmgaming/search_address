a0cc02fe5afba784b0e5dc1e818952e28383f1a5
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-03 23:07:15 +0700

    update home ui show current latitude

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nqmgaming.searchaddresslab"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
